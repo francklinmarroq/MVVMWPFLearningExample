81b964561e8c812f06a1338dc2c3994efeca7df2
Francklin Marroquin <francklin.marroquin@outlook.com>
2022-12-04 03:41:45 -0600

    WPF Navigation but not working yet
    Sill needs a fix to let radiobuttons actually change the view.

---
+++ Trader/State/Navigators/INavigator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Trader.ViewModels;
+
+namespace Trader.State.Navigators
+{
+    public enum ViewType
+    {
+        Home,
+        Portfolio
+    }
+    public interface INavigator
+    {
+        ViewModelBase CurrentViewModel { get; set; }
+        ICommand UpdateCurrentViewModelCommand { get; }
+    }
+}
---
+++ Trader/State/Navigators/Navigator.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using Trader.Commands;
+using Trader.Models;
+using Trader.ViewModels;
+
+namespace Trader.State.Navigators
+{
+    public class Navigator : ObservableObject, INavigator
+    {
+        private ViewModelBase _currentViewModel;
+        public ViewModelBase CurrentViewModel
+        {
+            get
+            {
+                return _currentViewModel;
+            }
+            set
+            {
+                _currentViewModel = value;
+                OnPropertyChanged(nameof(_currentViewModel));
+            }
+        }
+        public ICommand UpdateCurrentViewModelCommand => new UpdateCurrentViewModelCommand(this);
+    }
+}
